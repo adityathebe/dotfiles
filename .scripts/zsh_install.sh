b29ac6ed3fb9a7b2dde9610b244f0bd79e5dc04a
Aditya <thebeaditya@gmail.com>
2019-09-01 18:09:00 +0545

    ZSH && oh-my-zsh installation scripts

---
+++ .scripts/zsh_install.sh
@@ -0,0 +1,15 @@
+# Install zsh
+sudo apt install zsh
+
+# Change default shell to zsh
+chsh -s $(which zsh)
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install Plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+# Install powerlevel9k theme
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
