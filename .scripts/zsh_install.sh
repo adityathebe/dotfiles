6c5a49cf4ca3cc593a1b754ac460f10ec98bdd51
Aditya <thebeaditya@gmail.com>
2019-12-23 22:11:03 +0545

    echo statements

--- .scripts/zsh_install.sh
+++ .scripts/zsh_install.sh
@@ -1,13 +1,19 @@
+sudo apt install
+
 # Install zsh
-sudo apt install zsh
+echo "\n\n\nInstalling ZSH"
+sudo apt install zsh -y
 
 # Change default shell to zsh
+echo "\n\n\nChanging default shell to zsh"
 chsh -s $(which zsh)
 
 # Install oh-my-zsh
+echo "\n\n\nInstalling oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Install Plugins
+echo "\n\n\nInstalling plugins"
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
