53d18db92b17460f1e0b6ac6a7e6124919462f9f
Aditya <thebeaditya@gmail.com>
2019-12-28 00:34:11 +0545

    Fixed typo. Select Config ==> Select Song

--- .scripts/rofi/list-songs.sh
+++ .scripts/rofi/list-songs.sh
@@ -3,7 +3,7 @@
 # I use this script with rofi to enumerate all the mp3 and m4a files in ~/Music directory
 # The selected song will be played with ffplay
 
-song=$(find "$HOME/Music" -type f \( -name "*.mp3" -o -name "*.m4a" \) | rofi -dmenu -i -lines 5 -width 50 -p "Config File : " -font "Overpass 15")
+song=$(find "$HOME/Music" -type f \( -name "*.mp3" -o -name "*.m4a" \) | rofi -dmenu -i -lines 5 -width 50 -p "Select Song : " -font "Overpass 15")
 
 ffplay -autoexit "$song"
 
