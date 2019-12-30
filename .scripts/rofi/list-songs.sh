0d4558c5e75a2fe75a33607ee70a5774d486883f
Aditya <thebeaditya@gmail.com>
2019-12-30 15:36:27 +0545

    Changed rofi menu width

--- .scripts/rofi/list-songs.sh
+++ .scripts/rofi/list-songs.sh
@@ -3,7 +3,8 @@
 # I use this script with rofi to enumerate all the mp3 and m4a files in ~/Music directory
 # The selected song will be played with ffplay
 
-song=$(find "$HOME/Music" -type f \( -name "*.mp3" -o -name "*.m4a" \) | rofi -dmenu -i -lines 5 -width 50 -p "Select Song : " -font "Overpass 15")
+song=$(find "$HOME/Music" -type f \( -name "*.mp3" -o -name "*.m4a" \) | rofi -dmenu -i -lines 5 -width 80 -p "Select Song : " -font "Overpass 15")
+
 
 ffplay -autoexit "$song"
 
