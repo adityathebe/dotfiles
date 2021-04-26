b2a14c54d7b09914c87aa2c414f4ca6c20fdba9e
Aditya <thebeaditya@gmail.com>
2021-04-26 15:51:18 +0545

    launch different bars on different monitor

--- .config/polybar/polybar_launch.sh
+++ .config/polybar/polybar_launch.sh
@@ -8,8 +8,13 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-	MONITOR=$m polybar --reload top-bar &
-	MONITOR=$m polybar --reload bottom-bar &
+	bar="main"
+	if [[ $m == "eDP-1" ]]; then
+		bar="secondary"
+	fi
+
+	MONITOR=$m polybar -c "~/.config/polybar/config.ini" --reload "$bar" &
+	# MONITOR=$m polybar --reload bottom-bar &
 done
 
 echo "Polybar launched..."
