762361a21baf9b806a7dfce87ba9d344e0cd8aa0
Aditya Thebe <thebeaditya@gmail.com>
2024-01-29 22:35:56 +0545

    chore: update restic backup script

--- .local/share/restic-backup/backup.sh
+++ .local/share/restic-backup/backup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-source "$HOME/.config/secrets"
+source "/home/gunners/.config/secrets"
 
 # Backup config dir
-restic backup $HOME/.config --exclude-file="$HOME/.local/share/restic-backup/excludes.txt"
+restic backup /home/gunners/.config /etc --exclude-file="/home/gunners/.local/share/restic-backup/excludes.txt" -v
 
 # Remove old backups
 restic forget --keep-last 100 --prune
