ee5b45ada0e59ea8f4db1a802b47752ea622306f
Aditya Thebe <thebeaditya@gmail.com>
2024-01-30 11:36:14 +0545

    chore: update restic backup script

--- .local/share/restic-backup/backup.sh
+++ .local/share/restic-backup/backup.sh
@@ -2,8 +2,11 @@
 
 source "/home/gunners/.config/secrets"
 
+# Generated pacman packages
+pacman -Q > "/home/gunners/.local/share/pacman-packages/$(date +'%Y-%m-%d-%H-%M-%S').txt"
+
 # Backup config dir
-restic backup /home/gunners/.config /etc --exclude-file="/home/gunners/.local/share/restic-backup/excludes.txt" -v
+restic backup --files-from="/home/gunners/.local/share/restic-backup/includes.txt" --exclude-file="/home/gunners/.local/share/restic-backup/excludes.txt" -v
 
 # Remove old backups
-restic forget --keep-last 100 --prune
+restic forget --keep-last 200 --prune
