a4426556e29d2d0fc8afdcad709ecb62d60b03cd
Aditya Thebe <contact@adityathebe.com>
2024-03-24 11:59:51 +0545

    cleanup env vars and fix monitor name change

--- .config/polybar/polybar_launch.sh
+++ .config/polybar/polybar_launch.sh
@@ -9,12 +9,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch Polybar, using default config location ~/.config/polybar/config
 for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
 	bar="main"
-	# if [[ $m == "eDP-1" ]]; then
-	# 	bar="secondary"
-	# fi
-
 	MONITOR=$m polybar -c "~/.config/polybar/config.ini" --reload "$bar" &
-	# MONITOR=$m polybar --reload bottom-bar &
 done
 
 echo "Polybar launched..."
