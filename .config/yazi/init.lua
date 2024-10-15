a848da0a1ead2ade3c8bde805d4443f41621cc96
Aditya Thebe <contact@adityathebe.com>
2024-10-15 21:12:16 +0545

    yazi: speed up browsing network mounted directories

---
+++ .config/yazi/init.lua
@@ -0,0 +1,15 @@
+-- Show user/group of files in status bar
+-- https://yazi-rs.github.io/docs/tips#user-group-in-status
+Status:children_add(function()
+	local h = cx.active.current.hovered
+	if h == nil or ya.target_family() ~= "unix" then
+		return ui.Line({})
+	end
+
+	return ui.Line({
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+		ui.Span(":"),
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		ui.Span(" "),
+	})
+end, 500, Status.RIGHT)
